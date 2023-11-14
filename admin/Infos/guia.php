c4a64cfe75576b2af1d81e6094ae57c7c171abca
Allan_Vinicius <allan.vralmeida@senacsp.edu.br>
2023-11-14 17:01:31 -0300

    alteração 17.01

--- admin/Infos/guia.php
+++ admin/Infos/guia.php
@@ -11,7 +11,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/cabecalho.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/guia.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
     <title>Quem Somos - BuscaTec</title>
 </head>
 
@@ -25,85 +25,85 @@ require_once $_SERVER['DOCUMENT_ROOT'] . "/includes/cabecalho.php";
     <h2>1. Registro e Login</h2><br>
 
     <h3>Criando uma conta no BuscaTec</h3><br>
-    <ol>
+    <ul>
         <li>Acesse o site <a href="/admin/usuarios/cadastrar.php">BuscaTec</a>.</li>
         <li>Clique em "Registrar" ou "Criar Conta".</li>
         <li>Preencha o formulário com seu nome de usuário, endereço de e-mail e senha.</li>
         <li>Aceite os Termos de Serviço e Política de Privacidade.</li>
         <li>Clique em "Registrar" para concluir.</li>
-    </ol><br>
+    </ul><br>
 
     <h3>Fazendo o login na sua conta</h3><br>
-    <ol>
+    <ul>
         <li>Vá para a página inicial do <a href="">BuscaTec</a></li>
         <li>Clique em "Login" e insira seu nome de usuário ou endereço de e-mail e senha.</li>
         <li>Clique em "Entrar" para acessar sua conta.</li>
-    </ol><br>
-    
+    </ul><br>
+        
     <h1>2. Pesquisa de Profissionais</h1><br>
-    <ol>
+    <ul>
         <li>Na página inicial, utilize a barra de pesquisa.</li>
         <li>Digite as habilidades específicas desejadas (ex: "web development").</li>
         <li>Pressione Enter e revise os resultados.</li>
-    </ol><br>
+    </ul><br>
     <h3>Filtrando resultados</h3><br>
-    <ol>
+    <ul>
         <li>Refine sua pesquisa usando filtros como localização, avaliações, etc.</li>
         <li>Clique em "Filtrar" após ajustar suas preferências.</li>
-    </ol><br>
+    </ul><br>
     <h2>Visualizando perfis de profissionais</h2><br>
-    <ol>
+    <ul>
         <li>Explore os perfis dos <a href="">profissionais</a> listados</li>
         <li>Clique no nome de um profissional para visualizar detalhes completos.</li>
-    </ol><br>
+    </ul><br>
 
     <h1>3. Contratação de Profissionais</h1><br>
 
     <h4>Enviando mensagens aos profissionais</h4><br>
-    <ol>
+    <ul>
         <li>No perfil do profissional, clique em "Enviar Mensagem"</li>
         <li>Digite sua mensagem e clique em "Enviar".</li>
-    </ol><br>
+    </ul><br>
 
     <h3>Avaliando perfis e avaliações</h3><br>
-    <ol>
+    <ul>
         <li>Analise as habilidades, experiência e avaliações de outros usuários.</li>
         <li>Deixe uma avaliação após concluir uma contratação.</li>
-    </ol><br>
+    </ul><br>
     <h3>Finalizando a contratação</h3><br>
-    <ol>
+    <ul>
         <li>Após escolher um profissional, discuta os detalhes do trabalho.</li>
         <li>Finalize a contratação clicando em "Contratar" e siga as instruções.</li>
-    </ol><br>
+    </ul><br>
     <h1>4. Gerenciamento de Perfil</h1><br>
     <h4>Criando e atualizando seu perfil</h4><br>
-    <ol>
+    <ul>
         <li>Faça login e vá para a seção "Meu Perfil".</li>
         <li>Clique em "Editar Perfil" para adicionar informações e atualizar dados.</li>
-    </ol><br>
+    </ul><br>
     <h4>Gerenciando contas de usuários</h4><br>
-    <ol>
+    <ul>
         <li>Na página inicial, vá para "Configurações da Conta".</li>
         <li>Aqui você pode alterar sua senha, e-mail e outras configurações.</li>
-    </ol><br>
+    </ul><br>
     <h1>5. Termos de Uso e Sobre a BuscaTec </h1><br>
 
     <h4>Acesse informações detalhadas sobre a empresa BuscaTec</h4><br>
-    <ol>
+    <ul>
         <li>No rodapé do site, encontre os links para <a href="/assets/Itens/Termos de Uso da BuscaTec.pdf">Termos de Uso</a> e <a href="/admin/Infos/quemsomos.php">Quem Somos</a>.</li>
         <li>Leia as informações para entender melhor os serviços e políticas.</li>
-    </ol><br>
+    </ul><br>
     <h1>6. Perguntas Frequentes</h1><br>
     <h4>Dicas para uma busca bem-sucedida</h4><br>
-    <ol>
+    <ul>
         <li>Visite a seção de "<a href="">Dicas de Busca</a>" para orientações úteis.</li>
         <li>Utilize palavras-chave específicas para obter resultados mais precisos.</li>     
-    </ol><br>
+    </ul><br>
     <h3>Solução de problemas comuns</h3><br>
-    <ol>
+    <ul>
         <li>Confira a seção de "<a href="">Ajuda</a>" para solucionar problemas frequentes.</li>
         <li>Entre em contato com o suporte técnico se precisar de assistência adicional.</li>
-    </ol><br>
+    </ul><br>
     <p>Esperamos que este manual facilite sua experiência no BuscaTec. Se tiver dúvidas adicionais, consulte nossa seção de suporte ou entre em contato conosco diretamente.</p>
     </div>
 </body>
